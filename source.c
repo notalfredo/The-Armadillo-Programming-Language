02d505fa8812f140240a5ff380967460833030ae
David Miller <david.charles.miller@outlook.com>
2023-10-07 14:12:06 -0500

    Added example sourcecode file

---
+++ source.c
@@ -0,0 +1,64 @@
+// Single Line Comment
+
+/* 
+ * Multi 
+ * Line 
+ * Comment
+ */
+
+int a := 5;
+float b := 3.2;
+string c := "Hello, world!";
+
+// Interates over each element in iterable
+for (variable_name in iterable) {
+	
+}
+
+for (int i := 0; i < 10; i := i + 1) {
+
+}
+
+if (false) {
+
+} elif (false) {
+
+} elif (true) {
+
+} else {
+
+}
+
+if[3] (true), (file), (good_format) {
+	file := open("some.txt", "r");
+	// Condition check can be delayed until inside the body.
+	// Condition index starts at 0
+	check(1);
+	
+	for (line in file) {
+		good_format = check_format(line);
+		check(2); // Condition check can be repeated multiple times
+	}
+} else {
+	case 0 {
+	
+	}
+
+	case 1 {
+	
+	}
+
+	case 2 {
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
