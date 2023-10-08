979d364bd0e034853e5439d02d2275d8a1203a0d
Alfredo <icealfredo123@gmail.com>
2023-10-08 03:20:24 -0500

    codegen

---
+++ codegen.cpp
@@ -0,0 +1,70 @@
+#include "node.h"
+#include "codegen.h"
+#include "parser.hpp"
+
+using namespace std;
+
+/* Compile the AST into a module */
+void CodeGenContext::generateCode(NodeBlock& root) {
+	std::cout << "Generating code...\n";
+	
+	/* Create the top level interpreter function to call as entry */
+	vector<Type*> argTypes;
+	FunctionType *ftype = FunctionType::get(Type::getVoidTy(MyContext), makeArrayRef(argTypes), false);
+	mainFunction = Function::Create(ftype, GlobalValue::InternalLinkage, "main", module);
+	BasicBlock *bblock = BasicBlock::Create(MyContext, "entry", mainFunction, 0);
+	
+	/* Push a new variable/block context */
+	pushBlock(bblock);
+	root.codeGen(*this); /* emit bytecode for the toplevel block */
+	ReturnInst::Create(MyContext, bblock);
+	popBlock();
+	
+	/* Print the bytecode in a human-readable format 
+	   to see if our program compiled properly
+	 */
+	std::cout << "Code is generated.\n";
+	// module->dump();
+
+	legacy::PassManager pm;
+	// TODO:
+	pm.add(createPrintModulePass(outs()));
+	pm.run(*module);
+}
+
+/* Executes the AST by running the main function */
+GenericValue CodeGenContext::runCode() {
+	std::cout << "Running code...\n";
+	ExecutionEngine *ee = EngineBuilder( unique_ptr<Module>(module) ).create();
+	ee->finalizeObject();
+	vector<GenericValue> noargs;
+	GenericValue v = ee->runFunction(mainFunction, noargs);
+	std::cout << "Code was run.\n";
+	return v;
+}
+
+
+/* Returns an LLVM type based on the identifier */
+static Type *typeOf(const std::string type) {
+
+	if (type.name.compare("integer") == 0) {
+		return Type::getInt64Ty(MyContext);
+	}
+	else if (type.name.compare("real") == 0) {
+		return Type::getDoubleTy(MyContext);
+	}
+
+	return Type::getVoidTy(MyContext);
+}
+
+
+
+
+
+
+
+
+
+
+
+
